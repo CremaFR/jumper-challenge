933c60c0a396d6b87c6bd2c3500d60d3906bb9ea
Thomas Brillard <thomb45@gmail.com>
2025-05-25 18:00:47 +0200

    feat: simple metamask button - adding viem, wagmi styled components deps

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,32 @@
-import { Box, Typography } from "@mui/material";
-import Image from "next/image";
+'use client'
+
+import { createConfig, http } from 'wagmi'
+import { mainnet } from 'wagmi/chains'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+import { injected } from 'wagmi/connectors'
+import { MetaMaskConnect } from '@/components/walletSelector/MetaMaskConnect'
+
+// Create a wagmi config with only MetaMask
+const config = createConfig({
+  chains: [mainnet],
+  transports: {
+    [mainnet.id]: http(),
+  },
+  connectors: [
+    injected({ target: 'metaMask' }), // Specifically target MetaMask
+  ],
+})
+
+// Create a React Query client
+const queryClient = new QueryClient()
 
 export default function Home() {
   return (
-    <Box display="flex" justifyContent="center">
-      <Typography variant="h1">Welcome to Jumper challenge!</Typography>
-    </Box>
-  );
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+          <MetaMaskConnect config={config} />
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
 }
